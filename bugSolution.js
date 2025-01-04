9f1be870f7732be8582e5dff7580c9055d10f642
BugHunter-X <askzenai@gmail.com>
2025-01-04 19:50:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+//Solution using a fallback linear gradient:
+<div style="background-image: linear-gradient(to right, #6366f1, #9d61ed); background-size: cover; background-repeat: no-repeat; p-4 rounded-lg shadow-lg">
+  <p>This div uses a fallback linear-gradient for better cross-browser support.</p>
+</div>
+```
